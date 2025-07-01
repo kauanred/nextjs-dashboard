5243eacaca098f68b6531d4867b39faf536392a2
kauan_pereira2 <kauanmpereira1422@gmail.com>
2025-07-01 20:41:52 -0300

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-    return <p>Dashboard Page</p>
+import { lusitana } from '@/app/ui/fonts';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import { revenue } from '../lib/placeholder-data';
+
+export default async function Page() {
+    return(
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl}md:text-2x1`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            </div>
+            <div className="mt-6 grif gap-6 sm:grid-cols-2 lg:grid-cols-8">
+            <RevenueChart revenue={revenue}/>
+            </div>
+        </main>
+    )
 }
